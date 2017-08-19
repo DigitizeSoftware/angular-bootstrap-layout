10fc2c86ee830385eb99cefe952b441dfa8d4f6e
Iurii Raboshuk <iurii.raboshuk@gmail.com>
2017-08-19 05:16:44 +0300

    Basic gulp setup

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+/**
+ * Copyright (c) 2017 Digitize OÜ. Subject to MIT license.
+ */
+
+var gulp = require('gulp');
+
+gulp.task('default', function() {
+    // place code for your default task here
+});
